7c4c20c7eb5a54c82222d33341d8bcc8f2e86962
Martin Arce <martin.arce.teixeira@gmail.com>
2023-05-21 22:23:07 +0200

    ttt

---
+++ chikiaddin/src/taskpane/taskpane.ts
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
+ * See LICENSE in the project root for license information.
+ */
+
+/* global document, Office */
+
+Office.onReady((info) => {
+  console.log('GOT!')
+  if (info.host === Office.HostType.Outlook) {
+    console.log('GOT 2')
+    document.getElementById("sideload-msg").style.display = "none";
+    document.getElementById("app-body").style.display = "flex";
+    document.getElementById("run").onclick = run;
+  }
+});
+
+export async function run() {
+  /**
+   * Insert your Outlook code here
+   */
+}
