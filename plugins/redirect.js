0a0776495bcb19d563e7e3cf12cd41966e9c8d6c
Martin Arce <martin.arce.teixeira@gmail.com>
2023-05-29 13:39:45 +0200

    k

--- plugins/redirect.js
+++ plugins/redirect.js
@@ -5,11 +5,11 @@ export default function ({ route, redirect }) {
   console.log(redirect);
   if (route.path === "/index.html") {
     // return redirect("/");
-    window.location.href = "/";
+    window.location.href = "/addin";
   }
   if (route.path === "/meow") {
     console.log("omeooww!");
-    window.location.href = "/";
+    window.location.href = "/addin";
     // redirect(301, "/");
     console.log("after meow!");
   }
