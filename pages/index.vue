b5f9fd1b1c73c1f57ded1cce0cb7ae18be40c549
Martin Arce <martin.arce.teixeira@gmail.com>
2023-05-29 02:36:09 +0200

    s

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,41 @@
+
 <template>
 <h1>Nuxt appp loaded</h1>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+import { ref, onMounted } from 'vue';
+declare global {
+  interface Window {
+    Office: any;
+  }
+}
 
 export default Vue.extend({
-  name: 'IndexPage'
+  name: 'IndexPage',
+  setup() {
+    const waitForOfficeAvailability = () => {
+      return new Promise<void>((resolve) => {
+        const checkAvailability = () => {
+          if (typeof window.Office !== 'undefined') {
+            resolve();
+          } else {
+            setTimeout(checkAvailability, 100);
+          }
+        };
+
+        checkAvailability();
+      });
+    };
+    onMounted(async () => {
+      await waitForOfficeAvailability();
+
+      window.Office.onReady(() => {
+        console.log('Office ready');
+        // Code to be executed when the Office API is ready
+      });
+    });
+  },
 })
 </script>
--- plugins/office.js
+++ plugins/office.js
@@ -2,7 +2,6 @@ console.log("Carga office plugin");
 
 export default ({ app }) => {
   if (process.client) {
-    console.log("Meojow!h");
     const script = document.createElement("script");
     script.src = "https://appsforoffice.microsoft.com/lib/1/hosted/office.js";
     script.async = false;
