9c2f1d36e7909134b8125251d4f1ee634fd57e00
Martin Arce <martin.arce.teixeira@gmail.com>
2023-05-29 12:08:51 +0200

    404

---
+++ pages/404.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>Redirecting...</div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    if (process.client) {
+      window.location.href = "/addin/";
+    }
+  },
+};
+</script>
